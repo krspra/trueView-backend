6ec418188231eb90718144fecb6cb8f3d59acf0b
Pradeep Yadav <pradeepyd708@gmail.com>
2025-01-22 16:13:11 +0530

    is it working

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,23 +1,23 @@
 import { auth } from "../lib/firebaseAdmin.js";
 
-const checkAuthenticated=async(req,res,next)=>{
-    try {
-        const accessToken = await req.cookies?.accessToken;
-        console.log(accessToken);
-        if (!accessToken) {
-            return res.status(401).json({ message: "No token provided", success: false });
-        }
-
-        const decodedToken = await auth.verifyIdToken(accessToken);
-
-        if (!decodedToken?.email) {
-            throw new Error("Invalid token payload");
-        }
-        next();
-    } catch (error) {
-        console.error("Error verifying token:", error.message);
-        return res.status(401).json({ message: "Unauthorized", success: false });
+const checkAuthenticated = async (req, res, next) => {
+  try {
+    const accessToken = req.headers?.authorization?.split(' ')[1];
+    if (!accessToken) {
+      return res.status(401).json({ message: "No token provided", success: false });
     }
-}
+
+    const decodedToken = await auth.verifyIdToken(accessToken);
+
+    if (!decodedToken?.email) {
+      throw new Error("Invalid token payload");
+    }
+
+    next();
+  } catch (error) {
+    console.error("Error verifying token:", error.message);
+    return res.status(401).json({ message: "Unauthorized", success: false });
+  }
+};
 
 export default checkAuthenticated;
