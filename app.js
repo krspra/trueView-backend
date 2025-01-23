ac41006b258836a165c974fef8bc4fc1e4160611
Pradeep Yadav <pradeepyd708@gmail.com>
2025-01-23 12:34:51 +0530

    changes maded

--- app.js
+++ app.js
@@ -6,18 +6,33 @@ import ratingRouter from "./routes/rating.route.js";
 import checkAuthenticated from "./middleware/authentication.js";
 
 const app = express();
+
 const corsOptions = {
-  origin:"https://true-view-frontend.vercel.app",
+  origin: ["https://true-view-frontend.vercel.app"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization","Cookie"],
-}
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api/user/",checkAuthenticated, userRouter);
-app.use("/api/rating/",checkAuthenticated,ratingRouter);
+
+app.use((req, res, next) => {
+  const allowedReferers = ["https://true-view-frontend.vercel.app"];
+  const referer = req.headers.referer;
+
+  if (!referer || !allowedReferers.some((allowed) => referer.startsWith(allowed))) {
+    return res.status(403).json({ message: "Forbidden: Invalid Referer" });
+  }
+
+  next();
+});
+
+// Routes
+app.use("/api/user/", checkAuthenticated, userRouter);
+app.use("/api/rating/", checkAuthenticated, ratingRouter);
 
 export default app;
